971fe3dda0ee18933a5e2c74094d562418477984
muhammad naufal <muhammadnaufal321@students.amikom.ac.id>
2023-06-05 22:45:06 +0700

    enkapsulasi

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace programenkapsulasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("program data proplayer mlbb");
+            Console.WriteLine("===========================");
+
+            Proplayer proplayer1 = new Proplayer();
+            Proplayer proplayer2 = new Proplayer();
+            Proplayer proplayer3 = new Proplayer();
+
+            proplayer1.SetData("Lemon", 24, "RRQ Hoshi");
+            proplayer1.SetDataRole("MidLaner", "kagura", 1);
+            Console.WriteLine();
+
+            proplayer2.SetData("King Branz bin Jabran", 21, "Evos legend");
+            proplayer2.SetDataRole("GoldLaner", "Karie", 4);
+            Console.WriteLine();
+
+            proplayer3.SetData("Celiboy", 22, "Alter Ego");
+            proplayer3.SetDataRole("Jungler", "Baxilit", 3);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Proplayer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace programenkapsulasi
+{
+    internal class Proplayer
+    {
+        // fields
+        public string nama;
+        public string tim;
+        public string role;
+        public string turnamen;
+        public int umur;
+        public int season;
+
+        // constructor
+        public Proplayer() { }
+
+        public Proplayer(string nama, string tim, string role, int umur, int season)
+        {
+            this.nama = nama;
+            this.tim = tim;
+            this.role = role;
+            this.umur = umur;
+            this.season = season;
+        }
+
+
+        // method
+        public void SetData(string nama, int umur, string tim)
+        {
+            this.nama = nama;
+            this.umur = umur;
+            this.tim = tim;
+            Console.WriteLine("nama proplayer     : {0}", this.nama);
+            Console.WriteLine("umur proplayer     : {0}", this.umur);
+            Console.WriteLine("nama tim           : {0}", this.tim);
+        }
+
+        public void SetDataRole(string turnamen, string role, int season)
+        {
+            this.turnamen = turnamen;
+            this.role = role;
+            this.season = season;
+            Console.WriteLine("role               : {0}", this.turnamen);
+            Console.WriteLine("hero faforit       : {0}", this.role);
+            Console.WriteLine("season main        : {0}", this.season);
+            Console.ReadKey();
+        }
+    }
+}
